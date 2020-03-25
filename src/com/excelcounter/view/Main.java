1f2aac21099f9a2266ff2f3d965e8f3d943de02c
Alexey <angerfist.aka.tr1ton@gmail.com>
2020-03-25 13:30:20 +0300

    main add row))

--- src/com/excelcounter/view/Main.java
+++ src/com/excelcounter/view/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static final String VERSION = "v0.4";
 
     public static void main(String[] args) {
+
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
